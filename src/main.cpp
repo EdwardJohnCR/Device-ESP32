467d7611b354cdaa194c5179088da5654ab08413
Kikilma <kikilma.tech@gmail.com>
2022-03-06 22:40:53 -0600

    WIFI Connection

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,71 @@
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 
+String dId = "010203";
+String webhook_pass = "Hizz7r2bZ1";
+String webhook_endpoint = "http://192.168.100.9:3001/api/getdevicecredentials";
+const char* mqtt_server = "192.168.100.9";
+
+//PINS 
+#define led 2
+
+//WiFi
+const char *wifi_ssid = "Terrolin";
+const char *wifi_password = "202128tiliga";
+
+//Functions definitions
+void clear();
+
 
 void setup() {
 
   Serial.begin(921600);
-  Serial.println("hola kikilma");
+  pinMode(led, OUTPUT);
+  clear();
+
+  Serial.print(underlinePurple + "\n\n\nWiFi Connection in Progress" + fontReset + Purple);
+
+  WiFi.begin(wifi_ssid,wifi_password);
+
+  int counter = 0;
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+    counter++;
+
+    if(counter > 10){
+      Serial.print("  ⤵" + fontReset);
+      Serial.print(Red + "\n\n         Ups WiFi Connection Failed :( ");
+      Serial.println(" -> Restarting..." + fontReset);
+      delay(2000);
+      ESP.restart();
+    }
+
+  }
+
+    Serial.print("  ⤵" + fontReset);
+
+  //Printing local ip
+  Serial.println(boldGreen + "\n\n         WiFi Connection -> SUCCESS :)" + fontReset);
+  Serial.print("\n         Local IP -> ");
+  Serial.print(boldBlue);
+  Serial.print(WiFi.localIP());
+  Serial.println(fontReset);
+
   
 
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
+
+void clear()
+{
+  Serial.write(27);    // ESC command
+  Serial.print("[2J"); // clear screen command
+  Serial.write(27);
+  Serial.print("[H"); // cursor to home command
+}  
